e04659fe84eab75972470e0c588a7ca1df4a9c30
EthanVeselka <ethan.veselka@gmail.com>
2024-01-11 17:56:59 -0600

    fixed small layout discrepencies in benchmarks

--- torchmimic/benchmarks/decompensation/decompensation_benchmark.py
+++ torchmimic/benchmarks/decompensation/decompensation_benchmark.py
@@ -1,13 +1,13 @@
 import torch
 import torch.nn as nn
+
 from torch import optim
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 
+from torchmimic.loggers import DecompensationLogger
 from torchmimic.data import DecompensationDataset
 from torchmimic.utils import pad_colalte
-
-from torchmimic.loggers import DecompensationLogger
 
 
 class DecompensationBenchmark:
